c0b215096ab7b46045773cc98a408e6b1b47c254
kaisei <kaise64taka84@keio.jp>
2020-01-13 11:15:34 +0900

    係数を追加

--- model.py
+++ model.py
@@ -1,26 +1,26 @@
-import torch
-import torch.nn as nn
 import torch.nn.functional as F
+from dataset import *
 
 
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 3x3 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=3)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=3)
+        self.conv1_coef = nn.Parameter(torch.randn(64, device=device, dtype=dtype))
+        self.conv2_coef = nn.Parameter(torch.ones(192, device=device, dtype=dtype))
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+    def forward(self, x, flag):
+        conv1_coef = self.conv1_coef.unsqueeze(0).unsqueeze(2).unsqueeze(3)
+        conv2_coef = self.conv2_coef.unsqueeze(0).unsqueeze(2).unsqueeze(3)
+        x = F.max_pool2d(F.relu(self.conv1(x) * conv1_coef), (2, 2)) if flag is True \
+            else F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x) * conv2_coef), 2) if flag is True \
+            else F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -28,12 +28,8 @@ class Net(nn.Module):
         return x
 
     def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
+        size = x.size()[1:]
         num_features = 1
         for s in size:
             num_features *= s
         return num_features
-
-
-net = Net()
-print(net)
