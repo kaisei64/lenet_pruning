6aa760d6692b5148bf237c3c75aa21d018d9ea73
kaisei <kaise64taka84@keio.jp>
2020-01-13 12:47:10 +0900

    最初のコミット

---
+++ scripts/add_channel.py
@@ -0,0 +1,153 @@
+import os
+import sys
+pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(pardir)
+from channel_mask_generator import ChannelMaskGenerator
+from dense_mask_generator import DenseMaskGenerator
+from dataset import *
+from pfgacnn_mymodel import PfgaCnn
+from cnn_evaluateprune_mymodel import CnnEvaluatePrune
+from result_save_visualization import *
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+data_dict = {'val_loss': [], 'val_acc': []}
+# 枝刈り前パラメータ利用
+original_net = parameter_use('./result/pkl/original_train_epoch50.pkl')
+# 枝刈り前畳み込み層のリスト
+original_conv_list = [module for module in original_net.modules() if isinstance(module, nn.Conv2d)]
+# 枝刈り後パラメータ利用
+new_net = parameter_use('./result/pkl/dense_conv_prune_dense90per_10per.pkl')
+# 枝刈り後畳み込み層・全結合層・係数パラメータのリスト
+conv_list = [module for module in new_net.modules() if isinstance(module, nn.Conv2d)]
+dense_list = [module for module in new_net.modules() if isinstance(module, nn.Linear)]
+param_list = [module for module in new_net.modules() if isinstance(module, nn.Parameter)]
+# マスクのオブジェクト
+ch_mask = [ChannelMaskGenerator() for _ in range(len(conv_list))]
+for i, conv in enumerate(conv_list):
+    ch_mask[i].mask = np.where(np.abs(conv.weight.data.clone().cpu().detach().numpy()) == 0, 0, 1)
+de_mask = [DenseMaskGenerator() for _ in range(len(dense_list))]
+for i, dense in enumerate(dense_list):
+    de_mask[i].mask = np.where(np.abs(dense.weight.data.clone().cpu().detach().numpy()) == 0, 0, 1)
+
+gen_num = 30
+add_channel_num = 5
+optimizer = optim.SGD(new_net.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+
+# 追加前重み分布の描画
+for i in range(len(conv_list)):
+    before_weight = [np.sum(conv_list[i].weight.data[k].cpu().detach().numpy()) for k
+                     in range(len(conv_list[i].weight.data.cpu().numpy()))]
+    parameter_distribution_vis(f'./figure/dis_vis/conv{i + 1}/before_weight_distribution{i + 1}.png',
+                               before_weight)
+
+for count in range(add_channel_num):
+    ev = [CnnEvaluatePrune(count) for _ in range(len(conv_list))]
+    ga = [PfgaCnn(conv.in_channels, conv.kernel_size, i,
+                  evaluate_func=ev[i].evaluate, better_high=False, mutate_rate=0.1) for i, conv in enumerate(conv_list)]
+    best = [list() for _ in range(len(ga))]
+    for i in range(len(ga)):
+        # if i == 0 and count % 6 != 0 or i == 1 and count % 2 != 0 or i == 3 and count % 4 != 0 or i == 4 and count % 4 != 0:
+        #     continue
+        while ga[i].generation_num < gen_num:
+            ga[i].next_generation()
+            best[i] = ga[i].best_gene()
+            if best[i] is not None:
+                print(f'gen{i + 1}:{ga[i].generation_num} best-value{i + 1}:{best[i][1]}\n')
+
+        with torch.no_grad():
+            # 層ごとに１チャネルごと追加
+            for j in range(len(conv_list[i].weight.data.cpu().numpy())):
+                if np.sum(np.abs(ch_mask[i].mask[j])) < 25 * (count + 1) + 1:
+                    ch_mask[i].mask[j] = 1
+                    conv_list[i].weight.data[j] = torch.tensor(best[i][0], device=device, dtype=dtype)
+                    if i != len(conv_list) - 1:
+                        ch_mask[i + 1].mask[j, :] = 1
+                        conv_list[i + 1].weight.data[:, j] = original_conv_list[i + 1].weight.data[:, j].clone()
+                    break
+
+            # 追加後重み分布の描画
+            after_weight = [np.sum(conv_list[i].weight.data[k].cpu().numpy()) for k
+                            in range(len(conv_list[i].weight.data.cpu().numpy()))]
+            parameter_distribution_vis(
+                f'./figure/dis_vis/conv{i + 1}/after{count + 1}_weight_distribution{i + 1}.png', after_weight)
+
+            # 追加後チャネル可視化
+            # for j in range(conv_list[i].out_channels):
+            #     conv_vis(f'./figure/ch_vis_mymodel/conv{i + 1}/after{count + 1}_conv{i + 1}_filter{j + 1}.png'
+            #              , conv_list[i].weight.data.cpu().numpy(), j)
+
+        # パラメータの保存
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_10per.pkl', new_net)
+
+    for param in new_net.parameters():
+        param.requires_grad = False
+    for dense in dense_list:
+        dense.weight.requires_grad = True
+    for param in param_list:
+        param.requires_grad = True
+    f_num_epochs = 1
+    before_avg_val_loss, before_avg_val_acc = 0, 0
+    # finetune
+    for epoch in range(f_num_epochs):
+        # val
+        new_net.eval()
+        val_loss, val_acc = 0, 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                labels = labels.to(device)
+                outputs = new_net(images.to(device), True)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                val_acc += (outputs.max(1)[1] == labels).sum().item()
+        before_avg_val_loss, before_avg_val_acc = val_loss / len(test_loader.dataset), val_acc / len(test_loader.dataset)
+
+    for epoch in range(f_num_epochs):
+        # train
+        new_net.train()
+        train_loss, train_acc = 0, 0
+        for _, (images, labels) in enumerate(train_loader):
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = new_net(images, True)
+            loss = criterion(outputs, labels)
+            train_loss += loss.item()
+            train_acc += (outputs.max(1)[1] == labels).sum().item()
+            loss.backward()
+            optimizer.step()
+            for param in param_list:
+                param_max, param_min = torch.max(param), torch.min(param)
+                param = 2 * (param - param_min) / (param_max - param_min)
+                # print(param)
+                # print(torch.max(param))
+                # print(torch.min(param))
+            with torch.no_grad():
+                for j, dense in enumerate(dense_list):
+                    if de_mask[j].mask is None:
+                        break
+                    dense.weight.data *= torch.tensor(de_mask[j].mask, device=device, dtype=dtype)
+        avg_train_loss, avg_train_acc = train_loss / len(train_loader.dataset), train_acc / len(train_loader.dataset)
+
+        # val
+        new_net.eval()
+        val_loss, val_acc = 0, 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                labels = labels.to(device)
+                outputs = new_net(images.to(device), True)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                val_acc += (outputs.max(1)[1] == labels).sum().item()
+        avg_val_loss, avg_val_acc = val_loss / len(test_loader.dataset), val_acc / len(test_loader.dataset)
+
+        print(f'finetune, epoch [{epoch + 1}/{f_num_epochs}], train_loss: {avg_train_loss:.4f}'
+              f', train_acc: {avg_train_acc:.4f}, val_loss: {avg_val_loss:.4f}, val_acc: {avg_val_acc:.4f}')
+        print()
+
+        # 結果の保存
+        input_data = [before_avg_val_loss, before_avg_val_acc, avg_val_loss, avg_val_acc]
+        result_save('./result/csv/result_add_channels_retrain.csv', data_dict, input_data)
+        # パラメータの保存
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_10per.pkl', new_net)
---
+++ scripts/prune_dense_conv.py
@@ -0,0 +1,130 @@
+import os
+import sys
+pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(pardir)
+from channel_mask_generator import ChannelMaskGenerator
+from dataset import *
+from result_save_visualization import *
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import time
+
+data_dict = {'epoch': [], 'time': [], 'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
+
+# パラメータ利用, 全結合パラメータの凍結
+new_net = parameter_use('./result/pkl/dense_prune_90per.pkl')
+# 全結合層、畳み込み層のリスト
+dense_list = [module for module in new_net.modules() if isinstance(module, nn.Linear)]
+conv_list = [module for module in new_net.modules() if isinstance(module, nn.Conv2d)]
+conv_count = len(conv_list)
+# for dense in dense_list:
+#     dense.weight.requires_grad = False
+
+optimizer = optim.SGD(new_net.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+
+# マスクのオブジェクト
+ch_mask = [ChannelMaskGenerator() for _ in range(conv_count)]
+inv_prune_ratio = 10
+flag = [0 for i in range(3)]
+
+# channel_pruning
+for count in range(1, inv_prune_ratio + 9):
+    print(f'\nchannel pruning: {count}')
+    parameter_save('./result/pkl/middle_prune.pkl', new_net)
+    # ノルムの合計を保持
+    channel_l1norm_for_each_layer = [list() for _ in range(conv_count)]
+
+    # ノルムの取得, 昇順にソート
+    for i, conv in enumerate(conv_list):
+        channel_l1norm_for_each_layer[i] = [np.sum(torch.abs(param).cpu().detach().numpy()) for param in conv.weight]
+        channel_l1norm_for_each_layer[i].sort()
+
+    # 枝刈り本体
+    with torch.no_grad():
+        for i in range(len(conv_list)):
+            threshold = channel_l1norm_for_each_layer[i][int(conv_list[i].out_channels / inv_prune_ratio * count) - 1]\
+                if count <= 9 else channel_l1norm_for_each_layer[i][int(conv_list[i].out_channels *
+                                                        (9 / inv_prune_ratio + (count - 9) / inv_prune_ratio ** 2)) - 1]
+            save_mask = ch_mask[i].generate_mask(conv_list[i].weight.data.clone(),
+                                                 None if i == 0 else conv_list[i - 1].weight.data.clone(), threshold)
+            conv_list[i].weight.data *= torch.tensor(save_mask, device=device, dtype=dtype)
+
+    # パラメータの割合
+    weight_ratio = [np.count_nonzero(conv.weight.cpu().detach().numpy()) / np.size(conv.weight.cpu().detach().numpy())
+                    for conv in conv_list]
+
+    # 枝刈り後のチャネル数
+    channel_num_new = [conv.out_channels - ch_mask[i].channel_number(conv.weight) for i, conv in enumerate(conv_list)]
+
+    for i in range(conv_count):
+        print(f'conv{i + 1}_param: {weight_ratio[i]:.4f}', end=", " if i != conv_count - 1 else "\n")
+    for i in range(conv_count):
+        print(f'channel_number{i + 1}: {channel_num_new[i]}', end=", " if i != conv_count - 1 else "\n")
+
+    accuracy = 0
+    f_num_epochs = 5
+    # finetune
+    start = time.time()
+    for epoch in range(f_num_epochs):
+        # train
+        new_net.train()
+        train_loss, train_acc = 0, 0
+        for i, (images, labels) in enumerate(train_loader):
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = new_net(images, False)
+            loss = criterion(outputs, labels)
+            train_loss += loss.item()
+            train_acc += (outputs.max(1)[1] == labels).sum().item()
+            loss.backward()
+            optimizer.step()
+            with torch.no_grad():
+                for j, conv in enumerate(conv_list):
+                    conv.weight.data *= torch.tensor(ch_mask[j].mask, device=device, dtype=dtype)
+        avg_train_loss, avg_train_acc = train_loss / len(train_loader.dataset), train_acc / len(train_loader.dataset)
+
+        # val
+        new_net.eval()
+        val_loss, val_acc = 0, 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                labels = labels.to(device)
+                outputs = new_net(images.to(device), False)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                val_acc += (outputs.max(1)[1] == labels).sum().item()
+        avg_val_loss, avg_val_acc = val_loss / len(test_loader.dataset), val_acc / len(test_loader.dataset)
+        process_time = time.time() - start
+        accuracy = avg_val_acc
+
+        print(f'epoch [{epoch + 1}/{f_num_epochs}], time: {process_time:.4f}, train_loss: {avg_train_loss:.4f}'
+              f', train_acc: {avg_train_acc:.4f}, 'f'val_loss: {avg_val_loss:.4f}, val_acc: {avg_val_acc:.4f}')
+
+        # 結果の保存
+        input_data = [epoch + 1, process_time, avg_train_loss, avg_train_acc, avg_val_loss, avg_val_acc]
+        result_save('./result/csv/dense_conv_prune_parameter_dense90per_60per.csv', data_dict, input_data)
+
+    # パラメータの保存
+    if accuracy < 0.56 and flag[0] != 1:
+        new_net_save = parameter_use('./result/pkl/middle_prune.pkl')
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_60per.pkl', new_net_save)
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_60per_copy.pkl', new_net_save)
+        flag[0] = 1
+    elif accuracy < 0.36 and flag[1] != 1:
+        new_net_save = parameter_use('./result/pkl/middle_prune_.pkl')
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_40per.pkl', new_net_save)
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_40per_copy.pkl', new_net_save)
+        flag[1] = 1
+    elif accuracy < 0.16 and flag[2] != 1:
+        new_net_save = parameter_use('./result/pkl/middle_prune.pkl')
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_20per.pkl', new_net_save)
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_20per_copy.pkl', new_net_save)
+        flag[2] = 1
+    elif accuracy < 0.11:
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_10per.pkl', new_net)
+        parameter_save('./result/pkl/dense_conv_prune_dense90per_10per_copy.pkl', new_net)
+        break
+    else:
+        pass
